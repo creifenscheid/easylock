6485a2d59af0bfb630cc9c64c50625eccb60f4af
Christian Reifenscheid <sepp@einundzwanzig12.de>
2018-04-20 15:51:50 +0200

    [WIP] - refactoring js module

--- spl_easylock/Resources/Public/JavaScript/PasswordSecurityDisplay.js
+++ spl_easylock/Resources/Public/JavaScript/PasswordSecurityDisplay.js
@@ -11,6 +11,7 @@ define(function () {
      * @exports TYPO3/CMS/SplEasylock/PasswordSecurityDisplay
      */
     var PasswordSecurityDisplay = {};
+    var initCounter = 0;
 
     /**
      * initialize events
@@ -21,44 +22,84 @@ define(function () {
 
            // loop through every security level element to connect the corresponding input field
            $('.tx-spl-easylock-element-container').each(function(){
+
                // define objects
-               var inputField = $(this).find('.tx-spl-easylock-input');
+               var passwordField = $(this).find('.tx-spl-easylock-input');
                var securityLevel = $(this).find('.tx-spl-easylock-securitylevel');
 
+               // bind needed events to the input field
+               passwordField.on('change paste keyup', function(){
+                   var passwordFieldValue = passwordField.val();
 
-               console.log(inputField);
-               console.log(securityLevel);
+                   if (passwordFieldValue) {
+
+                       var passwordScore = PasswordSecurityDisplay.checkPasswordComplexity(passwordFieldValue);
+
+                       if (passwordScore > 75) {
+                           securityLevel.addClass("secure");
+                           securityLevel.removeClass("midsecure");
+                           securityLevel.removeClass("notsecure");
+                           securityLevel.text("Secure");
+                       } else if (passwordScore > 50) {
+                           securityLevel.addClass("midSecure");
+                           securityLevel.removeClass("secure");
+                           securityLevel.removeClass("notSecure");
+                           securityLevel.text("Could be more secure");
+                       } else {
+                           securityLevel.addClass("notSecure");
+                           securityLevel.removeClass("midSecure");
+                           securityLevel.removeClass("secure");
+                           securityLevel.text("Unsecure");
+                       }
+                   }
+               });
            });
+       }
+
+       else {
+
+           if (initCounter < 3) {
+
+               // call function again with delay
+               setTimeout(function(){
+                   $(PasswordSecurityDisplay.initializeEvents);
+               }, 1500);
+
+               // increase initCounter
+               initCounter++;
+           }
        }
     };
 
-    // call initialize events
-    $(PasswordSecurityDisplay.initializeEvents);
+    /**
+     * password complexity check
+     */
+    PasswordSecurityDisplay.checkPasswordComplexity = function (password) {
 
-    // return method object
-    return PasswordSecurityDisplay;
-});
-
-/*function checkPwd(pwdFieldId) {
-    var password = $('#' + pwdFieldId).val();
-    var passwordSecurityLevel = $('#tx-spl-easylock-securitylevel_' + pwdFieldId);
-
-    if (password) {
+        // init password score
         var pwdScore = 0;
+
+        var charLists = [
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            'abcdefghijklmnopqrstuvwxyz',
+            '0123456789',
+            '!@#$%&*()'
+        ];
 
         var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         var lowercase = "abcdefghijklmnopqrstuvwxyz";
         var digits = "0123456789";
         var splChars ="!@#$%&*()";
+        var minLenght = 8;
 
-        var ucaseFlag = contains(password, uppercase);
-        var lcaseFlag = contains(password, lowercase);
-        var digitsFlag = contains(password, digits);
-        var splCharsFlag = contains(password, splChars);
+        var ucaseFlag = PasswordSecurityDisplay.checkValue(password, uppercase);
+        var lcaseFlag = PasswordSecurityDisplay.checkValue(password, lowercase);
+        var digitsFlag = PasswordSecurityDisplay.checkValue(password, digits);
+        var splCharsFlag = PasswordSecurityDisplay.checkValue(password, splChars);
 
 
         // set password score
-        if (password.length>=8) {
+        if (password.length>=minLenght) {
             pwdScore += 40;
         }
 
@@ -78,31 +119,28 @@ define(function () {
             pwdScore += 15;
         }
 
-        if (pwdScore > 75) {
-            passwordSecurityLevel.addClass("secure");
-            passwordSecurityLevel.removeClass("midsecure");
-            passwordSecurityLevel.removeClass("notsecure");
-            passwordSecurityLevel.text("Secure");
-        } else if (pwdScore > 50) {
-            passwordSecurityLevel.addClass("midSecure");
-            passwordSecurityLevel.removeClass("secure");
-            passwordSecurityLevel.removeClass("notSecure");
-            passwordSecurityLevel.text("Could be more secure");
-        } else {
-            passwordSecurityLevel.addClass("notSecure");
-            passwordSecurityLevel.removeClass("midSecure");
-            passwordSecurityLevel.removeClass("secure");
-            passwordSecurityLevel.text("Unsecure");
-        }
-    }
-}
+        return pwdScore;
+    };
 
-function contains(password, allowedChars) {
-    for (var i = 0; i < password.length; i++) {
-        var char = password.charAt(i);
-        if (allowedChars.indexOf(char) >= 0) {
-            return true;
+    /**
+     * check value against predefined character list
+     */
+    PasswordSecurityDisplay.checkValue = function (value, charList) {
+
+        // loop through and compare every character of given value against the character list
+        for (var i = 0; i < value.length; i++) {
+            var char = value.charAt(i);
+            if (charList.indexOf(char) >= 0) {
+                return true;
+            }
         }
-    }
-    return false;
-}*/
+
+        return false;
+    };
+
+    // trigger init events
+    $(PasswordSecurityDisplay.initializeEvents);
+
+    // return method object
+    return PasswordSecurityDisplay;
+});

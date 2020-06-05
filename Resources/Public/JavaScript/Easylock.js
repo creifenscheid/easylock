b8a8cbc5eddcfe966f546c3c7ec74918e6d12900
Christian Reifenscheid <christian.reifenscheid.2112@gmail.com>
2020-06-05 20:20:35 +0200

    [TASK] Pwd generation
    Add js logic to generate pwd

--- Resources/Public/JavaScript/Easylock.js
+++ Resources/Public/JavaScript/Easylock.js
@@ -5,8 +5,22 @@ define([
 
     let Easylock = {};
 
-    Easylock.xxx = function () {
-        
+    Easylock.generatePwd = function () {
+        const minLength = 12;
+        const charList = [
+   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*','(',')','.',',',';',':','_','-'];
+   
+       const charListLength = charList.length;
+       
+       $('.tx-easylock.button').click(function(){
+           let generatedPwd = '';
+           
+           for (let i = 0; i < minLength; i++) {
+               generatedPwd += charList[Math.floor(Math.random() * charListLength)];
+           }
+           
+           $('.result').text(generatedPwd);
+        });
     };
 
     // expose to global

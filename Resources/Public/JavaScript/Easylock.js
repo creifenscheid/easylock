9dd9d9f0619cf2f7a7acfca315cf284c5d5bdbbc
Christian Reifenscheid <christian.reifenscheid.2112@gmail.com>
2020-06-07 13:54:37 +0200

    [TASK] refactoring
    Refactor js for pwd generation

--- Resources/Public/JavaScript/Easylock.js
+++ Resources/Public/JavaScript/Easylock.js
@@ -7,19 +7,26 @@ define([
 
     Easylock.generatePwd = function () {
         const minLength = 12;
-        const charList = [
-   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*','(',')','.',',',';',':','_','-'];
-   
-       const charListLength = charList.length;
-       
-       $('.tx-easylock.button').click(function(){
-           let generatedPwd = '';
-           
-           for (let i = 0; i < minLength; i++) {
-               generatedPwd += charList[Math.floor(Math.random() * charListLength)];
-           }
-           
-           $('.result').text(generatedPwd);
+        const charLists = [
+            ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+            ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+            ['0','1','2','3','4','5','6','7','8','9'],
+            ['!','@','#','$','%','&','*','(',')','.',',',';',':','_','-']
+        ];
+        
+        const charListsLength = charLists.length;
+        
+        $('.tx-easylock.button').click(function(){
+            let generatedPwd = '';
+            
+            for (let i = 0; i < minLength; i++) {
+                let charListId = Math.floor(Math.random() * charListsLength)
+                
+                let charListLength = charLists[charListId].length;
+                generatedPwd += charLists[charListId][Math.floor(Math.random() * charListLength)];
+            }
+            
+            $('.result').text(generatedPwd);
         });
     };
 
